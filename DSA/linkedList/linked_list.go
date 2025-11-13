0976fa54b9609cbdad3f7071804c95122d3620fb
Alex <alx5409@gmail.com>
2025-11-13 07:51:27 +0100

    Add more exercises about linked lists

--- DSA/linkedList/linked_list.go
+++ DSA/linkedList/linked_list.go
@@ -2,22 +2,55 @@ package linkedList
 
 import "fmt"
 
-// 5. Implement a singly linked list and write a function to reverse it.
+// 1. Implement a singly linked list
 
 type Node struct {
 	value int
 	next  *Node
 }
 
-// 6. Detect a cycle in a linked list.
+// 1. Check if the linked list is empty
 
-// 7. Implement a stack and use it to check for balanced parentheses in a string.
+// 2. Insert a node at the beginning of the linked list.
 
-// 8. Implement a queue and use it to simulate a simple task scheduler.
+// 3. Insert a node at the end of the linked list.
 
-// 9. Find the intersection node of two singly linked lists.
+// 4. Insert a node at a position in the linked list.
 
-// 14. Implement a function to merge two sorted linked lists.
+// 5. Delete a node at the beginning of the linked list.
+
+// 6. Delete a node at the end of the linked list.
+
+// 7. Delete a node by value.
+
+// 8. Clear a linked list
+
+// 9. Find the length of the linked list.
+
+// 10. Find the middle node of the linked list.
+
+// 11. Convert a linked list to a slice.
+
+// 12. Create a linked list from a slice.
+
+// 13. Print all elements of the linked list.
+
+// 14. Sort a linked list.
+
+// 15. Reverse a linked list
+
+// 16. Concatenate two linked lists
+
+// 17. Detect a cycle in a linked list.
+
+// 18. Find the intersection node of two singly linked lists.
+
+// 19. Implement a function to merge two sorted linked lists.
+
+// 20. Implement a stack and use it to check for balanced parentheses in a string.
+
+// 21. Implement a queue and use it to simulate a simple task scheduler.
+
 func main() {
 	fmt.Println("Linky")
 }
