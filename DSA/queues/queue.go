75ecdfbe6ce052fe3b0474842359358415c080bd
Alex <alx5409@gmail.com>
2025-11-25 08:42:03 +0100

    Add implementation of circular queue

--- DSA/queues/queue.go
+++ DSA/queues/queue.go
@@ -1,6 +1,9 @@
 package queue
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Queue DSA Practice Exercises:
 //
@@ -35,7 +38,6 @@ func (q *Queue[V]) Dequeue() (V, error) {
 	return val, nil
 }
 
-//
 // 2. Reverse a queue using recursion.
 func ReverseQueue[V any](queue Queue[V]) Queue[V] {
 	reversedQueue := Queue[V]{}
@@ -61,7 +63,58 @@ func recursiveReverseQueue[V any](queue Queue[V]) Queue[V] {
 // 3. Generate binary numbers from 1 to N using a queue.
 //
 // 4. Implement a circular queue.
-//
+type CircularQueue[V any] struct {
+	data  []V
+	cap   int
+	front int
+	rear  int
+	size  int
+}
+
+func NewCircularQueue[V any](capacity int) *CircularQueue[V] {
+	return &CircularQueue[V]{
+		data:  make([]V, capacity),
+		cap:   capacity,
+		front: 0,
+		rear:  0,
+		size:  0,
+	}
+}
+
+func (c CircularQueue[V]) IsEmpty() bool {
+	return c.size == 0
+}
+
+func (c *CircularQueue[V]) Enqueue(value V) error {
+	if c.size == c.cap {
+		return fmt.Errorf("max capacity reached, can not enqueue the value %v", value)
+	}
+	c.data[c.rear] = value
+	c.rear = (c.rear + 1) % c.cap
+	c.size++
+	return nil
+}
+
+func (c *CircularQueue[V]) Dequeue() (V, error) {
+	var result V
+	if c.IsEmpty() {
+		return result, errors.New("queue is empty, could not dequeue")
+	}
+	result = c.data[c.front]
+	c.front = (c.front + 1) % c.cap
+	c.size--
+	return result, nil
+}
+
+func (c *CircularQueue[V]) Peek() (V, error) {
+	var result V
+	if c.IsEmpty() {
+		return result, errors.New("queue is empty")
+	}
+	result = c.data[c.front]
+	return result, nil
+}
+
 // 5. Check if a given sequence of brackets can be balanced using a queue.
 //
 // 6. Implement a queue using two stacks.
