09f52620d1da172f521d8362d52370becd61c769
Alex <alx5409@gmail.com>
2026-01-02 22:42:58 +0100

    Add more generic types

--- DSA/generics/generic.go
+++ DSA/generics/generic.go
@@ -14,5 +14,41 @@ type Number interface {
 // Generic slice
 type Slice[T any] []T
 
-// Generic Map structure
-type Map[K comparable, V any] map[K]V
+// Generic hashmap
+type HashMap[K comparable, V any] map[K]V
+
+// Generic stack
+type Stack[V any] struct {
+	data []V
+}
+
+// Generic queue
+type Queue[V any] struct {
+	data []V
+}
+
+// Generic linked list node
+type ListNode[T any] struct {
+	Value T
+	Next  *ListNode[T]
+}
+
+// Generic linked list
+type LinkedList[T any] struct {
+	Head *ListNode[T]
+}
+
+// Generic binary tree node
+type TreeNode[T any] struct {
+	Value T
+	Left  *TreeNode[T]
+	Right *TreeNode[T]
+}
+
+// Generic binary tree
+type BinaryTree[T any] struct {
+	Root *TreeNode[T]
+}
+
+// Generic set
+type Set[T comparable] map[T]struct{} // Using empty struct to save memory (each value occupies 0 bytes)
