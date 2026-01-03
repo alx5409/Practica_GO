8775b7a2dddbad1e620214febd2efc2f8a812c1b
Alex <alx5409@gmail.com>
2026-01-03 22:45:55 +0100

    Add generic methods in the generic queue and a brief description of each method

--- DSA/generics/generic.go
+++ DSA/generics/generic.go
@@ -24,14 +24,17 @@ type Stack[V any] struct {
 	data []V
 }
 
+// IsEmpty returns true if the stack has no elements
 func (s Stack[V]) IsEmpty() bool {
 	return len(s.data) == 0
 }
 
+// Push adds a value to the top of the stack
 func (s *Stack[V]) Push(value V) {
 	s.data = append(s.data, value)
 }
 
+// Pop removes and returns the top value from the stack. Returns false if the stack is empty
 func (s *Stack[V]) Pop() (V, bool) {
 	if s.IsEmpty() {
 		var zero V
@@ -42,17 +45,48 @@ func (s *Stack[V]) Pop() (V, bool) {
 	return val, true
 }
 
+// Peek returns the top value of the stack without removing it. Returns an error if the stack is empty
 func (s *Stack[V]) Peek() (V, error) {
 	if s.IsEmpty() {
 		var zero V
-		return zero, errors.New("error, the stack is empty")
+		return zero, errors.New("the stack is empty")
 	}
 	return s.data[len(s.data)-1], nil
 }
 
 // Generic queue
 type Queue[V any] struct {
-	data []V
+	Data []V
+}
+
+// IsEmpty returns true if the queue has no elements.
+func (q Queue[V]) IsEmpty() bool {
+	return len(q.Data) == 0
+}
+
+// Peek returns the front value of the queue without removing it. Returns an error if the queue is empty
+func (q Queue[V]) Peek() (V, error) {
+	if q.IsEmpty() {
+		var zero V
+		return zero, errors.New("the queue is empty")
+	}
+	return q.Data[0], nil
+}
+
+// Enqueue adds a value to the end of the queue
+func (q *Queue[V]) Enqueue(value V) {
+	q.Data = append(q.Data, value)
+}
+
+// Dequeue removes and returns the front value from the queue. Returns an error if the queue is empty
+func (q *Queue[V]) Dequeue() (V, error) {
+	var val V
+	if q.IsEmpty() {
+		return val, errors.New("queue is empty")
+	}
+	val = q.Data[0]
+	q.Data = q.Data[1:]
+	return val, nil
 }
 
 // Generic linked list node
