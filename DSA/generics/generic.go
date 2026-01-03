fef6ba6868b89f26274a9076a4c95a74cd69a3d5
Alex <alx5409@gmail.com>
2026-01-03 22:42:52 +0100

    Add stack generic methods in the gneric file

--- DSA/generics/generic.go
+++ DSA/generics/generic.go
@@ -1,5 +1,7 @@
 package generics
 
+import "errors"
+
 // Generic Pair structure
 type Pair[A any] struct {
 	First  A
@@ -20,6 +22,32 @@ type HashMap[K comparable, V any] map[K]V
 // Generic stack
 type Stack[V any] struct {
 	data []V
+}
+
+func (s Stack[V]) IsEmpty() bool {
+	return len(s.data) == 0
+}
+
+func (s *Stack[V]) Push(value V) {
+	s.data = append(s.data, value)
+}
+
+func (s *Stack[V]) Pop() (V, bool) {
+	if s.IsEmpty() {
+		var zero V
+		return zero, false
+	}
+	val := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return val, true
+}
+
+func (s *Stack[V]) Peek() (V, error) {
+	if s.IsEmpty() {
+		var zero V
+		return zero, errors.New("error, the stack is empty")
+	}
+	return s.data[len(s.data)-1], nil
 }
 
 // Generic queue
